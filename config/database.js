75ccab8ea00163637ecb211f57e7a2ebea18f78b
humornetworking <andres.vasquez.perez@gmail.com>
2015-06-14 20:39:34 +0000

    Desarrollando desde c9.io

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 module.exports = {
 
 	// the database url to connect
-	url : 'mongodb://localhost/expl'
+	//url : 'mongodb://localhost/expl'
+	
+    url : 'mongodb://andres:unodos3@ds047612.mongolab.com:47612/explguru'
 }
