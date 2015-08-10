9d88c126c5880b1d871f45b9a52681bee65614b0
andresvasquez <holamundo.buz@gmail.com>
2015-08-10 06:47:30 -0300

    El placer del desacoplamiento   ahhhh.

--- config/setup.js
+++ config/setup.js
@@ -6,9 +6,9 @@ module.exports = {
 
 
 	// the database url to connect
-	'database' : 'mongodb://localhost/expl',
+	//'database' : 'mongodb://localhost/expl',
 
-	//'database' : 'mongodb://andres:unodos3@ds047612.mongolab.com:47612/explguru'
+	'database' : 'mongodb://andres:unodos3@ds047612.mongolab.com:47612/explguru',
 
 	'mail_api_key' : 'key-219426aefec7c90432a505766e1888bf',
     'mail_domain' : 'sandbox2576ebf851d144449cdb3023f5b14267.mailgun.org'
