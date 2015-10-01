7efad9ed2fdb5a808b7082d9d93e55b0fe7b860a
Andrés <holamundo.buz@gmail.com>
2015-09-30 22:42:20 -0300

    Se corrije este pequeño detalle

--- app/util/socket.js
+++ app/util/socket.js
@@ -7,7 +7,7 @@ module.exports = function (app,io,jwt) {
         socket.on('authenticate', function(data){
 
             var user = jwt.decode(data.token, app.get('superSecret'));
-            app.get('connections')[user.id] = socket;
+            app.get('connections')[user._id] = socket;
             socket.auth = true;
 
         });
